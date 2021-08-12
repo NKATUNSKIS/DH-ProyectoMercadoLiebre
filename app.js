d142bf53cf91e1e173a8a43a3e0b4aeda115e082
NKATUNSKIS <natalia.katunskis@gmail.com>
2021-08-11 23:39:00 -0300

    icono de envío y algunos cambios en el CSS

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const puerto = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, "./public")));
 const publicFolderPath = path.resolve(__dirname,"./public");
@@ -12,5 +13,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log("Server is running on PORT : 3000");
+  console.log('Server is running on PORT 3000');
 });
